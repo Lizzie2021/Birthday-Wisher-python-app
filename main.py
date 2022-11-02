006316c05d1eaa10eb036ddc7a5a412c0e7f084e
Lizzie2021 <liguanna2017@outlook.com>
2022-11-02 13:57:07 +1000

    initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+import pandas
+import random
+import smtplib
+MY_EMAIL = "guannali22@gmail.com"
+MY_PASSWORD = "uysjzparzzhxcijk"
+# Check if today matches a birthday in the birthdays.csv
+now = datetime.now()
+today = (now.month, now.day)
+data = pandas.read_csv("birthdays.csv")
+
+birthdays_dict = {(data_row.month, data_row.day): data_row for (index, data_row) in data.iterrows()}
+if today in birthdays_dict:
+    birthday_person = birthdays_dict[today]
+    file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        new_contents = contents.replace("[NAME]", birthday_person["name"])
+# Send the letter generated in last step to that person's email address.
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=birthday_person.email,
+            msg=f"Subject: Happy Birthday!\n\n{new_contents}"
+        )
+
+
+
